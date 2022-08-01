a5431aedfa8f461da27ed2f7fbc5765960f35f51
Luis Valgoi <luisvalgoi@gmail.com>
2022-08-01 13:58:42 -0300

    feat: added util fns

--- utils/Spreadsheet.ts
+++ utils/Spreadsheet.ts
@@ -1,3 +1,5 @@
+import { ISpreadsheetData } from 'components/Spreadsheet';
+
 export const LETTERS = [
   'A',
   'B',
@@ -34,7 +36,26 @@ export const LETTERS = [
 export const EMPTY_DATA = new Array(100).fill(new Array(LETTERS.length).fill(''));
 
 export const getData = (linkId: string) => {
-  const existingDataRaw = localStorage.getItem(linkId) as any as string;
-  const existingData = JSON.parse(existingDataRaw);
-  return existingData;
+  if (typeof window !== 'undefined') {
+    const existingDataRaw = localStorage?.getItem(linkId) as any as string;
+    const existingData = JSON.parse(existingDataRaw);
+    return existingData;
+  } else {
+    return {};
+  }
+};
+
+export const getReferenceCell = (value: string, source: ISpreadsheetData, circularRef: boolean): string => {
+  if (value?.startsWith('=') && circularRef) {
+    throw Error('Circular Ref');
+  }
+
+  if (value?.startsWith('=') && !circularRef) {
+    const cellValueHumanIndex = value.slice(1, value.length).toUpperCase();
+    const cellValue = source?.[cellValueHumanIndex]?.value;
+
+    return getReferenceCell(cellValue, source, cellValue === value);
+  }
+
+  return value;
 };
